e7cb98dee45b619e207580476f5b20ec0ec8e14a
Xinyue (Luna) <xinyuec57@gmail.com>
2023-10-23 12:38:03 -0400

    fix: prettier

--- packages/client/src/pages/Algorithm/HeapComponent/HeapComponent.jsx
+++ packages/client/src/pages/Algorithm/HeapComponent/HeapComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Animation from "./animate";
 import Heapification from "./heapmethod";
 const width = 600;
