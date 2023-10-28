835148c40ec7c87b8af9e83613b91d3bda343898
Fyutong <145179229+Fyutong@users.noreply.github.com>
2023-10-28 02:39:06 -0400

    update bst

--- packages/client/src/pages/Algorithm/bst.jsx
+++ packages/client/src/pages/Algorithm/bst.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect , useRef} from "react";
 import { Container, Box, Paper } from "@mui/material";
 import { AlgorithmSpace } from "./AlgComponent/algorithmSpace";
 import { ControlArea } from "./AlgComponent/controlArea";
-import BinarySearchTree from "./bstComponent/bstmethod";
-import Animation from "./bstComponent/bstanimate";
+import BinarySearchTree from "./bstComponent/bstmethod.js";
+import Animation from "./bstComponent/bstanimate.jsx";
 
 const width = 900;
 const height = 300;
@@ -11,8 +11,7 @@ var data = [4,7,8,2,1,3,5,9]
 var dataset = []
 var record = []
 var step = 0
-
-
+var tree = null
 
 function validdata(tdata){
   for (const ele of tdata) {
@@ -33,36 +32,86 @@ function datatran(data){
 
 function reset(){
   step = 0
-  record.forEach(element => {
-    console.log()
-    Animation.Pathdisappear(dataset[element-1].position)
+  dataset.forEach(element => {
+    //console.log(element.position)
+    Animation.Pathdisappear(element.position)
   })
+}
+
+function nextStep(){
+  if(step>=record.length)
+    {
+      alert("animation end")
+    }
+    else{
+      Animation.Pathdisplay(dataset[record[step]-1].position) 
+      step++   
+    }
+}
+
+function back(){
+  if(step<1)
+  {
+    alert("This is the first step!")
+  }
+  else
+  {
+    step--
+    Animation.Pathdisappear(dataset[record[step]-1].position) 
+  }
+}
+
+function Inorder(){
+  reset()
+  record = tree.inOrderTraverse()
+}
+
+function Preorder(){
+  reset()
+  record = tree.preOrderTraverse()
+}
+
+function Postorder(){
+  reset()
+  record = tree.postOrderTraverse()
 }
 
 const BST = () => {
   const svgRef = useRef(null);
 
-useEffect(() => {
-  Animation.createbst(dataset,svgRef);
-},[]);
-  const tree = new BinarySearchTree();
+  useEffect(() => {
+    Animation.createbst(dataset,svgRef);
+  },[]);
+
+  tree = new BinarySearchTree();
   datatran(data);
   dataset.forEach(element => {
     tree.insert(element, record);
   });
 
-  
-  dataset.forEach(element => {
-    console.log(element);
-  });
-
   return (
     <Container maxWidth="md">
       <Box className="canvas">
-      <div style={{ flexGrow: 1 }}>
-        <svg ref={svgRef} width={width} height={height}></svg>
+      <div style={{ display: 'flex' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <input id="create" placeholder="Enter comma separated numbers" />
+        </div>
+        <div style={{ flexGrow: 1 }}>
+          <svg ref={svgRef} width={width} height={height}></svg>
+          <div style={{ display: 'flex', justifyContent: 'middle', gap: '10px', marginTop: '10px' }}>
+          <button onClick={Inorder}>Inorder</button>
+          <button onClick={Preorder}>Preorder</button>
+          <button onClick={Postorder}>Postorder</button>
+          </div>
+        <div style={{ display: 'flex', justifyContent: 'middle', gap: '10px', marginTop: '10px' }}>
+          <button onClick={nextStep}>Next Step</button>
+          <button onClick={back}>Back</button>
+          <button onClick={reset}>Reset</button>
+          <button onClick={reset}>Final Bst</button>
+          </div>
+        </div>
+
       </div>
-        
       </Box>
     </Container>
   );
