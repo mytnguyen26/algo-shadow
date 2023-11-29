f918dd10741c9ea2e7cf087da1d8e09c60ea8cae
mytnguyen26 <49925370+mytnguyen26@users.noreply.github.com>
2023-11-29 14:58:53 -0500

    refactor: update pathDisplay function from yutong's animation module

--- packages/client/src/components/GraphRenderer.js
+++ packages/client/src/components/GraphRenderer.js
@@ -254,32 +254,17 @@ class GraphRenderer {
     document.getElementById("l" + size).style.display = "block";
   }
 
-  pathDisplay(index) {
-    const circle = document.getElementById("c" + index);
-    const line = document.getElementById("l" + index);
-  
-    // 设置圆形的渐变动画
+  pathDisplay(circle, attribute, color) {
+    // const circle = document.getElementById("c" + index).getElementsByTagName("ellipse")[0];
+    // Set up the circle gradient animation
     const circleAnimate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-    circleAnimate.setAttribute("attributeName", "stroke");
-    circleAnimate.setAttribute("values", "black;blue");
-    circleAnimate.setAttribute("dur", "2s");
+    circleAnimate.setAttribute("attributeName", attribute);
+    circleAnimate.setAttribute("values", color);
+    circleAnimate.setAttribute("dur", "1s");
     circleAnimate.setAttribute("fill", "freeze");
-  
+      
     circle.appendChild(circleAnimate);
     circleAnimate.beginElement();
-  
-    //设置线段的渐变动画
-    if (index !== 1) {
-      const lineAnimate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-      lineAnimate.setAttribute("attributeName", "stroke");
-      lineAnimate.setAttribute("values", "black;blue");
-      lineAnimate.setAttribute("fill", "freeze");
-      lineAnimate.setAttribute("dur", "5s");
-      lineAnimate.setAttribute("fill", "freeze");
-      lineAnimate.setAttribute("begin", "c" + index + ".animate.end");
-      line.appendChild(lineAnimate);
-      lineAnimate.beginElement();
-    }
   }
 
   pathDisappear(index) {
