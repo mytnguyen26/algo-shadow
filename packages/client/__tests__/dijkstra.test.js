c92b79d737c76d0988cd9f431cfdf19ad2d06d5c
Xinyue (Luna) <xinyuec57@gmail.com>
2023-12-01 18:44:55 -0500

    fix: remove component test

--- packages/client/__tests__/dijkstra.test.js
+++ packages/client/__tests__/dijkstra.test.js
@@ -1,14 +1,14 @@
-import { DijkstraGraph } from "../src/pages/Algorithm/DijkstraComponent/DijkstraGraph.jsx";
+// import { DijkstraGraph } from "../src/pages/Algorithm/DijkstraComponent/DijkstraGraph.jsx";
 import { expect, it, describe } from "vitest";
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import Graph from "../src/components/AlgorithmSolver/algorithm.js";
 
-describe(DijkstraGraph, () => {
-  it("should render correctly", () => {
-    const result = render();
-    expect(result).toMatchSnapshot();
-  });
-});
+// describe(DijkstraGraph, () => {
+//   it("should render correctly", () => {
+//     const result = render();
+//     expect(result).toMatchSnapshot();
+//   });
+// });
 
 describe("DijkstraAlgorithm", () => {
   it("should get correct shortest path", () => {
