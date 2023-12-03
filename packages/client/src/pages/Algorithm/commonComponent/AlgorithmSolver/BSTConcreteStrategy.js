e085f441331cbcb49f4efc4f66085fdc697e4d5d
mytnguyen26 <49925370+mytnguyen26@users.noreply.github.com>
2023-12-03 15:06:04 -0500

    fix: BST inorder traverse

--- packages/client/src/pages/Algorithm/commonComponent/AlgorithmSolver/BSTConcreteStrategy.js
+++ packages/client/src/pages/Algorithm/commonComponent/AlgorithmSolver/BSTConcreteStrategy.js
@@ -42,30 +42,24 @@ class BSTConcreteStrategy {
       if (newNode.nodeData.value < node.nodeData.value) {
         // 如果插入的节点值比父节点小则插入到左节点上反之则插入到右节点上
         if (node.left === null) {
-          console.log("input data", data)
-          console.log("insert position", node)
-          console.log("data position", data.position)
           data.position = position * 2
           newNode.nodeData.position = data.position
           node.left = newNode
           record.push(position)
           record.push(data.position)
         } else {
-          record.push(node.position)
+          record.push(node.nodeData.position)
           insertNode(node.left, newNode, position * 2) // 递归找下一层的左侧节点（重点）
         }
       } else {
         if (node.right === null) {
-          console.log("input data", data)
-          console.log("insert position", node)
-          console.log("data position", data.position)
           data.position = position * 2 + 1
           newNode.nodeData.position = data.position
           node.right = newNode
           record.push(position)
           record.push(data.position)
         } else {
-          record.push(node.position)
+          record.push(node.nodeData.position)
           insertNode(node.right, newNode, position * 2 + 1)
         }
       }
@@ -81,20 +75,17 @@ class BSTConcreteStrategy {
 
   // Inorder iterates over all nodes（left root right）
   inOrderTraverse() {
-    console.log()
     let backs = []
     const callback = (data) => {
       return data
     }
     const inOrderNode = (node, callback) => {
-      console.log("Node", node)
       if (node !== null) {
         inOrderNode(node.left, callback) // 递归遍历出左节点
-        backs.push(callback(node.position)) // 将值push到数组里
+        backs.push(callback(node.nodeData.position)) // 将值push到数组里
         inOrderNode(node.right, callback) // 递归遍历出右节点
       }
     }
-    console.log("Root", this.root)
     inOrderNode(this.root, callback)
     return backs
   }
@@ -107,7 +98,7 @@ class BSTConcreteStrategy {
     }
     const inOrderNode = (node, callback) => {
       if (node !== null) {
-        backs.push(callback(node.position)) // 将值push到数组里
+        backs.push(callback(node.nodeData.position)) // 将值push到数组里
         inOrderNode(node.left, callback) // 递归遍历出左节点
         inOrderNode(node.right, callback) // 递归遍历出右节点
       }
@@ -126,7 +117,7 @@ class BSTConcreteStrategy {
       if (node !== null) {
         inOrderNode(node.left, callback) // 递归遍历出左节点
         inOrderNode(node.right, callback) // 递归遍历出右节点
-        backs.push(callback(node.position)) // 将值push到数组里
+        backs.push(callback(node.nodeData.position)) // 将值push到数组里
       }
     }
     inOrderNode(this.root, callback)
@@ -148,19 +139,21 @@ class BSTConcreteStrategy {
   }
 
   /**
-   * Traverse the tree to find the input `data`
+   * Traverse the tree to find the input `data`, then record the position
+   * of the node stored in nodeData.position to the `record` array
    * @param {*} data the node value the caller is looking for
    * @param {*} record the reference to the `record` array from `bst.jsx`
+   * to store the new node position order
    * @returns the Node data if it was found
    */
   search(data, record) {
     const searchNode = (node) => {
       if (node === null) return false
       if (node.nodeData.value == data) {
-        record.push(node.position)
+        record.push(node.nodeData.position)
         return node
       }
-      record.push(node.position)
+      record.push(node.nodeData.position)
       return searchNode(data < node.nodeData.value ? node.left : node.right, data)
     }
     return searchNode(this.root, data)
@@ -182,16 +175,16 @@ class BSTConcreteStrategy {
         if (node.left !== null && node.right !== null) {
           let _node = this.min(node.right)
           node.nodeData.value = _node.nodeData.value
-          localRecord.push(node.position)
+          localRecord.push(node.nodeData.position)
           node.right = removeNode(node.right, dData)
           return node
         }
       } else if (dData < node.nodeData.value) {
-        localRecord.push(node.position)
+        localRecord.push(node.nodeData.position)
         node.left = removeNode(node.left, dData)
         return node
       } else {
-        localRecord.push(node.position)
+        localRecord.push(node.nodeData.position)
         node.right = removeNode(node.right, dData)
         return node
       }
