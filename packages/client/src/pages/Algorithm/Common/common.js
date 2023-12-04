12395642231482dd1b18f689420b1ad69551ecd1
mytnguyen26 <49925370+mytnguyen26@users.noreply.github.com>
2023-12-04 14:22:40 -0500

    refactor: for Common module, add JSDoc. Change CAnimation component name to TreeGraphRenderer component name. change variable names and function names to camelCase

--- packages/client/src/pages/Algorithm/Common/common.js
+++ packages/client/src/pages/Algorithm/Common/common.js
@@ -1,11 +1,19 @@
-import AnimationB from "../bstComponent/bstanimate.jsx";
-import CAnimation from "./Canimate.js";
+/**
+ * TODO
+ */
+import TreeGraphRenderer from "./Canimate.js";
 
-const Common= {
+const Common = {
     width : 900,
     height : 300,
-    validData(valuename){
-        let tdata = document.getElementById(valuename).value.split(",")
+
+    /**
+     * TODO
+     * @param {*} valueName 
+     * @returns 
+     */
+    validData(valueName){
+        let tdata = document.getElementById(valueName).value.split(",")
         for (const ele of tdata) {
           if (isNaN(ele)) {
             throw new Error("Only numbers and commas can be entered");
@@ -13,16 +21,28 @@ const Common= {
         }
         return tdata
     },
-      
-    validOneData(valuename){
-        let xdata =  Common.validData(valuename)
+    
+    /**
+     * TODO
+     * @param {*} valueName 
+     * @returns 
+     */
+    validOneData(valueName){
+        let xdata =  Common.validData(valueName)
         if (xdata.length === 1) {
             return xdata[0]
         }else {
             throw new Error("Only select one number");
         }
     },
-    findInArray(xdata,dataset) {
+
+    /**
+     * TODO
+     * @param {*} xdata 
+     * @param {*} dataset 
+     * @returns 
+     */
+    findInArray(xdata, dataset) {
         for (var i = 0; i < dataset.length; i++) {
           if (dataset[i].value == xdata) {
             return i;
@@ -30,28 +50,41 @@ const Common= {
         }
         throw new Error(xdata + " is not in heap");
     },
-    nextStep(step,record){
+
+    /**
+     * TODO
+     * @param {*} step 
+     * @param {*} record 
+     * @returns 
+     */
+    next(step, record){
       if(step>=record.length)
       {
         alert("Animation is end!")
-        
       }
       else
       {
         if(record[step].e1==0){
-          CAnimation.deleteelement(record[step].e2[1],record[step].e2[0])
+          TreeGraphRenderer.deleteElement(record[step].e2[1],record[step].e2[0])
         }
         else{
           const text1 = document.getElementById("t" + record[step].e1);
           const text2 = document.getElementById("t" + record[step].e2);
-          CAnimation.animateExchange(text1,text2);
+          TreeGraphRenderer.animateExchange(text1, text2);
         }
         ++step
       }
       return step
     },
 
-    back(step,record,...restParams){
+    /**
+     * TODO
+     * @param {*} step 
+     * @param {*} record 
+     * @param  {...any} restParams 
+     * @returns 
+     */
+    back(step, record, ...restParams){
       if(step<1)
       {
         alert("This is the first step!")
@@ -60,16 +93,16 @@ const Common= {
       {
         step--
         if(typeof(record[step].e1) == "undefined"){
-          AnimationB.Pathdisappear(record[step]);
+          TreeGraphRenderer.pathDisappear(record[step]);
         }
         else{
           if(record[step].e1==0){
-            CAnimation.showelement(record[step].e2[1],record[step].e2[0])
+            TreeGraphRenderer.showElement(record[step].e2[1],record[step].e2[0])
           }
           else{
             const text1 = document.getElementById("t" + record[step].e1);
             const text2 = document.getElementById("t" + record[step].e2);
-            CAnimation.animateExchange(text1,text2);
+            TreeGraphRenderer.animateExchange(text1,text2);
           }
         }
       }
